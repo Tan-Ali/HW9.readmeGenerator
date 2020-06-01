7a5c184facbf4266d1cd2bdd2ff2d26fdcb8e1c3
Tanner Nielsen <tan-ali5@outlook.com>
2020-06-01 11:09:50 -0700

    questions

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 const questions = [
 
+    {
+        type: "input",
+        name: "gitHub",
+        message: "What is your gitHub username?"
+    };
+    {
+        type: "input",
+        name: "gitHub",
+        message: "What is your gitHub username?"
+    }
+    {;
+        type: "input",
+        name: "gitHub",
+        message: "What is your gitHub username?"
+    };
+    {
+        type: "input",
+        name: "gitHub",
+        message: "What is your gitHub username?"
+    };
+    {
+        type: "input",
+        name: "gitHub",
+        message: "What is your gitHub username?"
+    };
 ];
 
 function writeToFile(fileName, data) {
